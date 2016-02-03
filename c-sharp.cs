4a8f4f9cf4c51cddabbaf2c3b25e5e554a147018
Alexander Dutton <gh@alexdutton.co.uk>
2016-02-03 09:00:49 +0000

    Added C# implementation

---
+++ c-sharp.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asciify
+{
+	static class Asciify
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (asciify("Héllöþ☺"));
+		}
+
+		private static Dictionary<char,String> specialCases = new Dictionary<char,String> () {
+			{'æ', "ae"},
+			{'Æ', "AE"},
+			{'œ', "oe"},
+			{'Œ', "OE"},
+			{'þ', "th"},
+			{'Þ', "TH"},
+			{'ä', "ae"},
+			{'Ä', "AE"},
+			{'ö', "oe"},
+			{'Ö', "OE"},
+			{'ü', "ue"},
+			{'Ü', "UE"},
+		};
+
+		public static String asciify(String text) {
+			text = text.Normalize (System.Text.NormalizationForm.FormC);
+			StringBuilder newText = new StringBuilder ();
+			foreach (char c in text) {
+				newText.Append (specialCases.ContainsKey(c) ? specialCases[c] : c.ToString ());
+			}
+
+			text = newText.ToString().Normalize (System.Text.NormalizationForm.FormD);
+			newText = new StringBuilder ();
+			foreach (char c in text) {
+				if (c <= 127) {
+					newText.Append (c);
+				}
+			}
+			return newText.ToString ();
+		}
+	}
+}
